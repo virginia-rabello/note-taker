2881f22d20dad082d8c04730d27636834b1a8f4c
Virginia <virginia.rabello2107@gmail.com>
2021-05-28 13:36:58 -0500

    solving bug

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const fs = require('fs');
 const crypto = require('crypto');
 
 const app = express();
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 
 const {notes} = require('./db/db.json'); 
 
